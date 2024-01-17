5b9fd207fc0e5261afe10b457fe5aa9744218707
bs1462 <jobaer.ahamed@brainstation-23.com>
2024-01-17 12:24:47 +0600

    add forms and user account

--- app4/settings.py
+++ app4/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,8 @@ USE_I18N = True
 USE_TZ = True
 
 
+LOGIN_REDIRECT_URL ="home"
+LOGOUT_REDIRECT_URL ="home"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
